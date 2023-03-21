1adc0b3a8aed908d1dc5c40e1a12ef4afc7d1d17
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-03-21 07:43:59 +0400

    update build file and paths

--- dockers/mercurial/hg-server.sh
+++ dockers/mercurial/hg-server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ ! -d ./repos/.hg ]; then
-    hg init ./repos
-    cd ./repos
-    echo "print('Hello, World!')" > test.py
-    hg add test.py
-    hg commit -u "Daniel Sobrado <daniel@danielsobrado.com>" -m "commit message"
+# Change ownership of the /repos directory
+chown -R hguser:hguser /repos
+
+if [ ! -d ./.hg ]; then
+    # Run Mercurial commands as the hguser user
+    sudo -u hguser hg init .
+    sudo -u hguser echo "print('Hello, World!')" > test.py
+    sudo -u hguser hg add test.py
+    sudo -u hguser hg commit -u "Daniel Sobrado <daniel@danielsobrado.com>" -m "commit message"
 fi
 
 hg serve --daemon
